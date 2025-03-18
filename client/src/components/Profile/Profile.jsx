e81015e76c6d35df4a3d343167875c94490f3ad1
MOGGERte <100819819+MOGGERte@users.noreply.github.com>
2025-03-18 06:57:45 +0300

    Еще пару фиксов(посты не грузятся на страницу)

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { getUser, getUsers } from '../../api/profile/request.js';
 import { getNews } from '../../api/news/requests.js';
 import { Loading } from '../LoadingPage/';
+import { Post } from '../Post/Post.jsx';
 import s from './styles.module.css';
 
 export const Profile = () => {
@@ -11,18 +12,27 @@ export const Profile = () => {
   const [profiles, setProfiles] = useState([]);
   const [currentProfile, setCurrentProfile] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [userPost, setUserPost] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
     getUser(id)
       .then((profile) => {
+        console.log('Main profile:', profile);
         setCurrentProfile(profile);
         return getUsers();
       })
       .then((profiles) => {
+        console.log('Профили:', profiles);
         setProfiles(profiles);
-        setIsLoading(false);
         return getNews();
+      })
+      .then((news) => {
+        console.log('Новости:', news);
+        const userPosts = news.filter((post) => post.customerId === Number(id));
+        console.log('Посты:', userPosts);
+        setUserPost(userPosts);
+        setIsLoading(false);
       });
   }, [id]);
 
@@ -52,7 +62,20 @@ export const Profile = () => {
       <div className={s.mainPage}>
         <div className={s.postAndMusic}>
           <div className={s.musicContainer}>поле под музыку</div>
-          <div className={s.posts}>мои новости</div>
+          <div className={s.posts}>
+            {userPost.map((post) => (
+              <div key={post.id}>
+                <Post
+                  photoUrl={post.photoUrl}
+                  text={post.text}
+                  likesCounter={post.likesCounter}
+                  commentsCounter={post.commentsCounter}
+                  repostsCounter={post.repostsCounter}
+                />
+                пост
+              </div>
+            ))}
+          </div>
         </div>
         <div className={s.friendsListContainer}>
           <div className={s.friendsList}>
