3184568d8e6f8dc045f80c338a01d363b9656801
Klemeshov <dima-0510@mail.ru>
2025-03-22 15:24:24 +0500

    incapsulate likes logic

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { updatePostLike, getPostWithComments } from '../../api/posts/requests';
+import { getPostWithComments } from '../../api/posts/requests';
 import { PostHeader } from './PostHeader/PostHeader.jsx';
 import { PostContent } from './PostContent/PostContent.jsx';
 import { PostFooter } from './PostFooter/PostFooter.jsx';
@@ -9,8 +9,6 @@ import s from './styles.module.css';
 
 export const Post = ({ postId }) => {
   const [postData, setPostData] = useState(null);
-  const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState(0);
 
   const nav = useNavigate();
 
@@ -18,8 +16,6 @@ export const Post = ({ postId }) => {
     const fetchPostData = async () => {
       const post = await getPostWithComments(postId);
       setPostData(post);
-      setLikes(post.likesCounter || 0);
-      setIsLiked(post.likedInfo?.includes(0) || false);
     };
 
     fetchPostData();
@@ -29,13 +25,6 @@ export const Post = ({ postId }) => {
     nav(`/profile/${customerId}`);
   };
 
-  const onLikeClick = async () => {
-    const newState = !isLiked;
-    setIsLiked(newState);
-    setLikes((prev) => (newState ? prev + 1 : prev - 1));
-    updatePostLike(postId, newState, 0);
-  };
-
   if (!postData) return <div>Загрузка...</div>;
 
   return (
@@ -43,11 +32,11 @@ export const Post = ({ postId }) => {
       <PostHeader customer={postData.customer} onProfileClick={onProfileClick} />
       <PostContent photoUrl={postData.photoUrl} text={postData.text} />
       <PostFooter
-        likes={likes}
-        isLiked={isLiked}
-        onLikeClick={onLikeClick}
+        defaultLikes={postData.likesCounter}
+        defaultIsLiked={postData.likedInfo?.includes(0)}
         commentsCount={postData.comments.length}
         repostsCount={postData.repostsCounter}
+        postId={postId}
       />
       <PostComments comments={postData.comments} />
     </div>
--- client/src/components/Post/PostFooter/PostFooter.jsx
+++ client/src/components/Post/PostFooter/PostFooter.jsx
@@ -1,8 +1,26 @@
 import s from './styles.module.css';
-import { FaRegHeart, FaRegCommentAlt } from 'react-icons/fa';
+import { FaRegCommentAlt, FaRegHeart } from 'react-icons/fa';
 import { MdOutlineReply } from 'react-icons/md';
+import { useState } from 'react';
+import { updatePostLike } from '../../../api/posts/requests.js';
 
-export const PostFooter = ({ likes, isLiked, onLikeClick, commentsCount, repostsCount }) => {
+export const PostFooter = ({
+  commentsCount,
+  repostsCount,
+  postId,
+  defaultIsLiked,
+  defaultLikes
+}) => {
+  const [isLiked, setIsLiked] = useState(defaultIsLiked);
+  const [likes, setLikes] = useState(defaultLikes);
+
+  const onLikeClick = async () => {
+    const newState = !isLiked;
+    setIsLiked(newState);
+    setLikes((prev) => (newState ? prev + 1 : prev - 1));
+    updatePostLike(postId, newState, 0);
+  };
+
   return (
     <div className={s.footerContainer}>
       <div onClick={onLikeClick} className={`${s.footerCounterItem} ${isLiked ? s.liked : ''}`}>
